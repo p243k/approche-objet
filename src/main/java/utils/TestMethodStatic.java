91006743148b03ba27708b1407ab30c39675c4ab
Pierre MABUSO <pierremabuso@gmail.com>
2025-03-25 16:44:30 +0100

    TP4

---
+++ src/main/java/utils/TestMethodStatic.java
@@ -0,0 +1,14 @@
+package utils;
+import java.lang.Integer;
+
+public class TestMethodStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int chaine2 = Integer.parseInt(chaine);
+
+        int a = 10;
+        int b = 5;
+        int max = Integer.max(a, b);
+        System.out.println(max);
+    }
+}
