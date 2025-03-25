d0871467f073eef59928a8b36f99b7cc3eaf9124
Pierre MABUSO <pierremabuso@gmail.com>
2025-03-25 16:48:22 +0100

    TP5

---
+++ src/main/java/Compte.java
@@ -0,0 +1,25 @@
+public class Compte {
+    int compte;
+    int soldeCompte;
+
+    public Compte(int compte, int soldeCompte) {
+        this.compte = compte;
+        this.soldeCompte = soldeCompte;
+    }
+
+    public int getCompte() {
+        return compte;
+    }
+
+    public void setCompte(int compte) {
+        this.compte = compte;
+    }
+
+    public int getSoldeCompte() {
+        return soldeCompte;
+    }
+
+    public void setSoldeCompte(int soldeCompte) {
+        this.soldeCompte = soldeCompte;
+    }
+}
